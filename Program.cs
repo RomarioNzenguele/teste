719383675d49da985ed8fe971cd11782abc8d0c0
“Romário <nz.romariodasilva@gmail.com>
2025-01-03 16:04:16 +0100

    Add Sprint 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("\nHello, World!\n");
+
+Console.WriteLine("\nIniciando a Sprint 1\n");
